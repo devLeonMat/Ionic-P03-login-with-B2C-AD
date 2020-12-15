328147209d582859b5afa535a74eed5c33e5422e
leon matias <leonmatias1991@gmail.com>
2020-12-15 15:50:08 -0500

    save projetc

---
+++ src/app/views/tabs/tab1/tab1.module.ts
@@ -0,0 +1,25 @@
+import {IonicModule} from '@ionic/angular';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {Tab1Page} from './tab1.page';
+
+
+import {Tab1PageRoutingModule} from './tab1-routing.module';
+import {AppModule} from "../../../app.module";
+import {HeaderComponent} from "../../../components/header/header.component";
+
+@NgModule({
+  imports: [
+    IonicModule,
+    CommonModule,
+    FormsModule,
+    Tab1PageRoutingModule
+  ],
+  exports: [
+    HeaderComponent
+  ],
+  declarations: [Tab1Page, HeaderComponent]
+})
+export class Tab1PageModule {
+}
